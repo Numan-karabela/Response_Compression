6f44e05b7d40663028f2eaf27bb5160ff58e89cc
1453n <1453n@DESKTOP-PHGFDVP>
2024-08-15 15:50:13 +0300

    Add project files.

---
+++ Response_Compression/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true; 
+    //options.Providers.Add<GzipCompressionProvider>();
+});
+
+builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.Fastest;
+});
+ 
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseResponseCompression();
+app.UseHttpsRedirection();
+
+app.MapGet("/",()=>{ 
+
+    List<int> list = new List<int>();
+
+	for (int i = 0; i < 10000; i++)
+	{
+		list.Add(i);
+	}
+	return list;
+
+});
+ 
+app.Run();
+
+ 
